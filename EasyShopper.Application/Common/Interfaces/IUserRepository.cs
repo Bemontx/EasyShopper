039dfa77d90feb0a8046908bb9c092cfe220b9a8
Jeisson Cardoso <101949185+Bemontx@users.noreply.github.com>
2026-02-03 12:15:17 -0600

    interfaces User/folders structure

---
+++ EasyShopper.Application/Common/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using EasyShopper.Domain.Entities;
+
+namespace EasyShopper.Application.Common.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetByEmailAsync(string email);
+    Task<User?> GetByIdAsync(Guid id);
+    Task AddAsync(User user);
+}
