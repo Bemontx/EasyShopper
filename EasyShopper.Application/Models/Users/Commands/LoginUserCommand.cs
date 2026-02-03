88f4326081cbe0812b8a42135bfbf4e9ef39b0d0
Jeisson Cardoso <101949185+Bemontx@users.noreply.github.com>
2026-02-03 12:22:26 -0600

    Commands User

---
+++ EasyShopper.Application/Models/Users/Commands/LoginUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace EasyShopper.Application.Users.Commands;
+
+public class LoginUserCommand : IRequest<Guid?>
+{
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+}
---
+++ EasyShopper.Application/Models/Users/Commands/RegisterUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace EasyShopper.Application.Users.Commands;
+
+public class RegisterUserCommand : IRequest<Guid>
+{
+    public string Name { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+}
