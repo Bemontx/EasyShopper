e223b16bf1291d585fc2cc7b1b2a9b92a9afd134
Jeisson Cardoso <101949185+Bemontx@users.noreply.github.com>
2026-02-03 15:17:57 -0600

    update inyection applications/config program import

---
+++ EasyShopper.Application/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using System.Reflection;
+
+namespace EasyShopper.Application.DependencyInjection;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly())
+        );
+
+        return services;
+    }
+}
--- EasyShopper/Program.cs
+++ EasyShopper/Program.cs
@@ -1,19 +1,15 @@
-using EasyShopper.Application;
+using EasyShopper.Application.DependencyInjection;
 using EasyShopper.Infrastructure.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Application
-//builder.Services.AddApplication();
-builder.Services.AddControllers();
-builder.Services.AddOpenApi();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Application (CQRS, MediatR, Validators)
+builder.Services.AddApplication();
 
-// Infrastructure
+// Infrastructure (EF, Identity, Repos, DbContext)
 builder.Services.AddInfrastructure(builder.Configuration);
 
-// Controllers
+// API
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,18 +18,11 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseSwagger();
-app.UseSwaggerUI();
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
