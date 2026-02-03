80e41d1f6d60786280f2df0a59a53eacec95632d
Jeisson Cardoso <101949185+Bemontx@users.noreply.github.com>
2026-02-03 14:06:28 -0600

    add Identity service

--- EasyShopper.Application/Common/Interfaces/IAuthService.cs
+++ EasyShopper.Application/Common/Interfaces/IAuthService.cs
@@ -1,4 +1,6 @@
-﻿public interface IAuthService
+﻿namespace EasyShopper.Application.Common.Interfaces;
+
+public interface IAuthService
 {
     Task<Guid?> LoginAsync(string email, string password);
     Task<Guid> RegisterAsync(string name, string email, string password);
