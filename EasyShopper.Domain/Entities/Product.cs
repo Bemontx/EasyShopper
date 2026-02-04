44ba356254e09b83fee9cdaa05f61d59ad63c282
Jeisson Cardoso <101949185+Bemontx@users.noreply.github.com>
2026-02-04 15:05:17 -0600

    add constructor entitie Product

--- EasyShopper.Domain/Entities/Product.cs
+++ EasyShopper.Domain/Entities/Product.cs
@@ -6,4 +6,22 @@ public class Product
     public string Name { get; set; } = default!;
     public decimal Price { get; set; }
     public string ImageUrl { get; set; } = default!;
+
+    // Constructor vacío para EF
+    private Product() { }
+
+    public Product(string name, decimal price, string imageUrl)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Price = price;
+        ImageUrl = imageUrl;
+    }
+
+    public void Update(string name, decimal price, string imageUrl)
+    {
+        Name = name;
+        Price = price;
+        ImageUrl = imageUrl;
+    }
 }
