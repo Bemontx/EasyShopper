ce233e8bd38190cadf5086b6aa3c43e9bd35d1a8
Jeisson Cardoso <101949185+Bemontx@users.noreply.github.com>
2026-02-04 10:07:19 -0600

    add ProductRepository/update Interface

--- EasyShopper.Application/Common/Interfaces/IProductRepository.cs
+++ EasyShopper.Application/Common/Interfaces/IProductRepository.cs
@@ -5,4 +5,5 @@ namespace EasyShopper.Application.Common.Interfaces;
 public interface IProductRepository
 {
     Task<IEnumerable<Product>> GetAllAsync();
+    Task<Product?> GetByIdAsync(Guid id);
 }
--- EasyShopper.Infrastructure/Repository/ProductRepository.cs
+++ EasyShopper.Infrastructure/Repository/ProductRepository.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EasyShopper.Application.Common.Interfaces;
+using EasyShopper.Domain.Entities;
+using EasyShopper.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
-namespace EasyShopper.Infrastructure.Repository
+namespace EasyShopper.Infrastructure.Repositories;
+
+public class ProductRepository : IProductRepository
 {
-    internal class ProductRepository
+    private readonly EasyShopperDbContext _context;
+
+    public ProductRepository(EasyShopperDbContext context)
     {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Product>> GetAllAsync()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    public async Task<Product?> GetByIdAsync(Guid id)
+    {
+        return await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
     }
 }
+
