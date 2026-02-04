c409bb8ebfd303141ce43eec5f253909f28eb154
Jeisson Cardoso <101949185+Bemontx@users.noreply.github.com>
2026-02-04 11:42:01 -0600

    add Bearer Authentication

--- EasyShopper/Program.cs
+++ EasyShopper/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddAuthentication(options =>
         ValidIssuer = jwtSettings["Issuer"],
         ValidAudience = jwtSettings["Audience"],
         IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(jwtSettings["Key"]!)
+            Encoding.UTF8.GetBytes(jwtSettings["Secret"]!)
         )
     };
 });
