0d386a611879b2a35ee602aaaca9afa292c30514
Jeisson Cardoso <101949185+Bemontx@users.noreply.github.com>
2026-02-04 12:56:47 -0600

    update controller authorize order/user no register

---
+++ EasyShopper/Controllers/OrdersController.cs
@@ -0,0 +1,44 @@
+﻿using EasyShopper.Application.Orders.Commands;
+using EasyShopper.Application.Orders.Commands.CreateOrder;
+using EasyShopper.Application.Orders.Queries.GetOrderById;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyShopper.Controllers;
+
+[ApiController]
+[Route("api/orders")]
+[Authorize]
+public class OrdersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public OrdersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateOrderCommand command)
+    {
+        var result = await _mediator.Send(command);
+
+        if (!result.IsSuccess)
+            return BadRequest(result.Error);
+
+        return Ok(result.Value);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var result = await _mediator.Send(
+            new GetOrderByIdQuery(id));
+
+        if (!result.IsSuccess)
+            return NotFound(result.Error);
+
+        return Ok(result.Value);
+    }
+}
