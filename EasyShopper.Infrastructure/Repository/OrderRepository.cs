c62cc572fc2ce30613aa5b3e70bb3d45fc803a36
Jeisson Cardoso <101949185+Bemontx@users.noreply.github.com>
2026-02-04 10:08:44 -0600

    update interface/add product repository

--- EasyShopper.Infrastructure/Repository/OrderRepository.cs
+++ EasyShopper.Infrastructure/Repository/OrderRepository.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EasyShopper.Application.Common.Interfaces;
+using EasyShopper.Domain.Entities;
+using EasyShopper.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
-namespace EasyShopper.Infrastructure.Repository
+namespace EasyShopper.Infrastructure.Repositories;
+
+public class OrderRepository : IOrderRepository
 {
-    internal class OrderRepository
+    private readonly EasyShopperDbContext _context;
+
+    public OrderRepository(EasyShopperDbContext context)
     {
+        _context = context;
+    }
+
+    public async Task AddAsync(Order order)
+    {
+        await _context.Orders.AddAsync(order);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Order>> GetByUserIdAsync(Guid userId)
+    {
+        return await _context.Orders
+            .Where(o => o.UserId == userId)
+            .ToListAsync();
     }
 }
