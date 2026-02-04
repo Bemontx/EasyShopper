eacac9c0cc6f521f12fa374d86122cd758d09390
Jeisson Cardoso <101949185+Bemontx@users.noreply.github.com>
2026-02-04 09:46:44 -0600

    update command/handler/register

--- EasyShopper.Application/Models/Users/Handlers/RegisterUserCommandHandler.cs
+++ EasyShopper.Application/Models/Users/Handlers/RegisterUserCommandHandler.cs
@@ -1,12 +1,12 @@
-﻿using MediatR;
-using EasyShopper.Application.Common.Interfaces;
+﻿using EasyShopper.Application.Common.Interfaces;
+using EasyShopper.Application.Common.Result;
 using EasyShopper.Application.Users.Commands;
 using EasyShopper.Domain.Entities;
+using MediatR;
 
 namespace EasyShopper.Application.Users.Handlers;
 
-public class RegisterUserCommandHandler
-    : IRequestHandler<RegisterUserCommand, Guid>
+public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, Result<Guid>>
 {
     private readonly IUserRepository _userRepository;
 
@@ -15,19 +15,19 @@ public class RegisterUserCommandHandler
         _userRepository = userRepository;
     }
 
-    public async Task<Guid> Handle(
+    public async Task<Result<Guid>> Handle(
         RegisterUserCommand request,
         CancellationToken cancellationToken)
     {
         var existingUser = await _userRepository.GetByEmailAsync(request.Email);
 
         if (existingUser != null)
-            throw new Exception("User already exists");
+            return Result<Guid>.Failure("User already exists");
 
         var user = new User(request.Name, request.Email);
 
         await _userRepository.AddAsync(user);
 
-        return user.Id;
+        return Result<Guid>.Success(user.Id);
     }
 }
